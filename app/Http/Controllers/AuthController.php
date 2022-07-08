13f9006bdcb82d7c2839695e2bcea20d10b91ba7
unknown <ashishkhinju123456789@gmail.com>
2022-07-08 17:36:56 +0545

    changes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request){
+       
         $user = User::create([
             'name'=>$request->name,
             'email'=>$request->email,
